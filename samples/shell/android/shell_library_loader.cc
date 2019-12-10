db99856697b50ec85304140d61b94c68c2fc2970
brunoduan <bruno.duan@gmail.com>
2019-12-10 10:49:23 +0800

    Add jni registration for base and mojo

--- samples/shell/android/shell_library_loader.cc
+++ samples/shell/android/shell_library_loader.cc
@@ -3,9 +3,21 @@
 #include "base/android/library_loader/library_loader_hooks.h"
 #include "base/bind.h"
 
+#include "samples/shell/android/samples_shell_jni_registration.h"
+
 // This is called by the VM when the shared library is first loaded.
 JNI_EXPORT jint JNI_OnLoad(JavaVM* vm, void* reserved) {
   base::android::InitVM(vm);
+  JNIEnv* env = base::android::AttachCurrentThread();
+  if (!RegisterMainDexNatives(env)) {
+    return -1;
+  }
+
+  bool is_browser_process =
+      !base::android::IsSelectiveJniRegistrationEnabled(env);
+  if (is_browser_process && !RegisterNonMainDexNatives(env)) {
+    return -1;
+  }
 
   return JNI_VERSION_1_4;
 }
---
+++ samples/shell/android/shell_manager.cc
@@ -0,0 +1,13 @@
+#include "samples/shell/android/shell_manager.h"
+
+#include "base/android/jni_android.h"
+#include "base/android/jni_string.h"
+#include "base/android/scoped_java_ref.h"
+
+
+namespace samples {
+
+void SamplesTest() {
+}
+
+}
